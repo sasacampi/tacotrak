9e01f941295bba4f64a60d21069f696746de49e3
Vinicius Oliveira Queiroz <vinniqueiroz03@gmail.com>
2023-09-01 16:13:50 -0300

    Navbar semi-completa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,5 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div>
+        <header>
+            <div className="navbar--logo">
+            <h1 class="title" >Tacotrak</h1>         
+                  </div>
+                  <label for="nav-toggle" className = "nav-toggle-label"> 
+                    <span className="label-span"></span> 
+                  </label>
+                  <nav> 
+                    <ul>                      
+                      <li><a>Home</a></li>
+                      <li><a>Sobre nós</a></li>
+                      <li><a>Menu</a></li>
+                      <li><a>Ferramentas</a></li>
+                      <li><a>Contato</a></li> 
+                      <li><button><h1>Entrar</h1></button></li>
+                    </ul>
+                  </nav>
+                  
+                </header>
+                  <div class="content">
+                  </div>
+                </div>
+
+    )
 }
