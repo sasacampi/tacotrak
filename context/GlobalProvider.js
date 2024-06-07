5423b045ca99c627f3895e0e1fad11d7f4bdcb83
Sabrina <sabrinapscampos@gmail.com>
2024-06-07 16:15:27 -0300

    feat: makes the login work

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -9,7 +9,46 @@ const GlobalProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Verificar se o usuário está logado
+    const checkUserSession = async () => {
+      let token = null;
+
+      // Verificar se localStorage está disponível
+      if (typeof window !== "undefined" && window.localStorage) {
+        token = localStorage.getItem("userToken");
+      }
+
+      if (token) {
+        try {
+          const response = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          if (response.ok) {
+            const userData = await response.json();
+            setUser(userData);
+            setIsLogged(true);
+          } else {
+            setIsLogged(false);
+            setUser(null);
+          }
+        } catch (error) {
+          console.error("Erro ao verificar a sessão do usuário:", error);
+          setIsLogged(false);
+          setUser(null);
+        }
+      } else {
+        setIsLogged(false);
+        setUser(null);
+      }
+
+      setLoading(false);
+    };
+
+    checkUserSession();
   }, []);
 
   return (
